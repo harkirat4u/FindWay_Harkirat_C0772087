d483e8f8f9a766717d006e1a414d3ab9068bf415
harkirat singh <singhharkirat051@gmail.com>
2020-06-12 10:31:08 -0400

    error in location

--- FindWay_Harkirat_C0772087/ViewController.swift
+++ FindWay_Harkirat_C0772087/ViewController.swift
@@ -7,14 +7,65 @@
 //
 
 import UIKit
+import MapKit //module for Mapview
+import CoreLocation //module for to access location
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate,CLLocationManagerDelegate{
 
+    @IBOutlet weak var mapView: MKMapView!//outlet for mapView
+    let locationManager  = CLLocationManager()
     override func viewDidLoad() {
         super.viewDidLoad()
+        checklocationServices()
         // Do any additional setup after loading the view.
     }
+    func setupLocationManager(){
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    }
+    
+    func checklocationServices(){
+        if CLLocationManager.locationServicesEnabled(){
+            setupLocationManager()
+            checkAuthorization()
+        }
+    else{
+            //comeback
+        }
+    }
+    func checkAuthorization(){
+        switch CLLocationManager.authorizationStatus(){
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        
+        case .denied:
+            break
+        case .authorizedAlways:
+            break
+        case .authorizedWhenInUse:
+          mapView.showsUserLocation = true
 
+            break
+        case .restricted:
+    
+            break
+        @unknown default:
+       
+            break
+        }
+        
+        
+    }
+    
+
+
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        //come back
+    }
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        //comeback
+    }
 
 }
 
